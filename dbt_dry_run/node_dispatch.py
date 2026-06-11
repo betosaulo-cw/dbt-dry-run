86a0423e24b33e231685f9528e4ce2ad94269eda
Alberto S Ferreira <alberto.ferreira@cloudwalk.io>
2026-06-11 18:31:39 -0300

    Revert "chore: update incremental materializations to use TableRunner"
    This reverts commit c530cf8c6aade39f361007e72cb38526bba2b03e.

--- dbt_dry_run/node_dispatch.py
+++ dbt_dry_run/node_dispatch.py
@@ -27,9 +27,9 @@ RUNNERS: Dict[RunnerKey, Type[NodeRunner]] = {
     RunnerKey("model", "fm_table"): TableRunner,
     RunnerKey("model", "fm_incremental"): TableRunner,
     RunnerKey("model", "fm_incremental_partitioned"): TableRunner,
-    RunnerKey("model", "incremental_partitioned"): TableRunner,
+    RunnerKey("model", "incremental_partitioned"): IncrementalRunner,
     # default materializations
-    RunnerKey("model", "incremental"): TableRunner, # test with table runner to avoid editor permissions
+    RunnerKey("model", "incremental"): IncrementalRunner,
     RunnerKey("model", "table"): TableRunner,
     RunnerKey("model", "view"): ViewRunner,
     RunnerKey("test", "test"): NodeTestRunner,
